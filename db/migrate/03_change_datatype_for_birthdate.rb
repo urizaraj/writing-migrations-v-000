d5e817b6bcfe491306ca531b691850d562269cea
Alexander Urizar <urizaraj@gmail.com>
2018-02-24 18:45:52 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,4 @@
-class ChangeBirthdayDateType < ActiveRecord::Migration
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration
   def change
     change_column :students, :birthdate, :datetime
   end
